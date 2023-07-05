0100906c4289f28eb810e82d2223157a91cd637f
Patrique Porto <patrikrock@hotmail.com>
2023-07-05 17:54:13 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -53,6 +53,20 @@
         }
     });
 
+    //Cookies storage
+    if (!localStorage.getItem("cookiesAccepted")) { 
+        var cookieMessage = document.getElementById('cookie-notification');  
+        var closeCookie = document.getElementById('cookie-notification-close');
+        
+        cookieMessage.style.display = 'block';  
+        closeCookie.addEventListener("click", function(e) {  
+          e.preventDefault();
+          localStorage.setItem("cookiesAccepted", true);
+          
+          cookieMessage.style.display = 'none';
+        });
+      }
+
      // Skills
      $('.skills').waypoint(function () {
         $('.progress .progress-bar').each(function () {
