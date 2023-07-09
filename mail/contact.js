6266accd1c66f211650a9d47a0f03fd1fbdc3bbf
Patrique Porto <patrikrock@hotmail.com>
2023-07-09 20:27:41 -0300

    Add files via upload

---
+++ mail/contact.js
@@ -0,0 +1,19 @@
+//Form validation
+
+$(function () {
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+ 
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
